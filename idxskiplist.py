eec480abefc0a0ee06723a4d9931cdfdd4d92111
Alexander Zhukov <zhukovaa90@gmail.com>
2013-03-30 16:13:06 +0200

    Added skiplist w/ indexing
    The copy of skiplist.py is created as idxsliplist.py.
    Code to maintian indexing has been remived from original
    file.

--- idxskiplist.py
+++ idxskiplist.py
@@ -13,11 +13,12 @@ class NIL(object):
 
 class skipnode(object):
 
-    def __init__(self, data, next):
+    def __init__(self, data, next, width):
         self.data = data
         self.next = next
+        self.width = width
         
-nil = skipnode(NIL(), [])
+nil = skipnode(NIL(), [], [])
         
 class skiplist:
 
@@ -30,7 +31,7 @@ class skiplist:
         self._max_levels = 1
         self._size = 0
         
-        self.head = skipnode('HEAD',[nil]*self._max_levels)
+        self.head = skipnode('HEAD',[nil]*self._max_levels,[1]*self._max_levels)
         self.distribution = distribution
         
     def __len__(self):
@@ -41,9 +42,15 @@ class skiplist:
                       for node in self])
         return '->'.join(s)
         
-    def __getitem__(self, data):
+    def __getitem__(self, i):
         '''Returns item with given index'''
-        return self.find(data)
+        node = self.head
+        i += 1
+        for level in reversed(range(self._max_levels)):
+            while node.width[level] <= i:
+                i -= node.width[level]
+                node = node.next[level]
+        return node.data
     
     def __iter__(self):
         'Iterate over values in sorted order'
@@ -67,26 +74,36 @@ class skiplist:
     def insert(self, data):
         '''Inserts data into appropriate position.'''
         update = [None]*self._max_levels
+        steps_at_level = [0]*self._max_levels
         node = self.head
         #find position to insert
         for level in reversed(range(self._max_levels)):
             while node.next[level].data <= data:
+                steps_at_level[level] += node.width[level]
                 node = node.next[level]
             update[level] = node
         node_height = self.distribution(self._p)
-        newnode = skipnode(data, [None]*node_height)
+        newnode = skipnode(data, [None]*node_height, [0]*node_height)
         steps = 0
         #if node's height is greater than number of levels
         #then add new levels, if not do nothing
         for level in range(self._max_levels, node_height):
             update.append(self.head)
+            self.head.width.append(len(self)+1)
             self.head.next.append(nil)
+            steps_at_level.append(0)
+            #update[level].width[level] += 1
         #insert node to each level <= node's height after
         #corresponding node in 'update' list
         for level in range(node_height):
             prevnode = update[level]
             newnode.next[level] = prevnode.next[level]
             prevnode.next[level] = newnode
+            newnode.width[level] = prevnode.width[level] - steps
+            prevnode.width[level] = steps + 1
+            steps += steps_at_level[level]
+        for level in range(node_height, self._max_levels):
+            update[level].width[level] += 1
         self._size += 1
         self._max_levels = max(self._max_levels, node_height)
 
@@ -107,8 +124,11 @@ class skiplist:
         node_height = len(node.next)
         for level in range(node_height):
             prevnode = update[level]
+            prevnode.width[level] += prevnode.next[level].width[level] - 1
             prevnode.next[level] = prevnode.next[level].next[level]
             
+        for level in range(node_height, self._max_levels):
+            update[level].width[level] -= 1
         while self._max_levels > 1 and self.head.next[level] == nil:
             self._max_levels -= 1
         del node
@@ -130,6 +150,6 @@ if __name__ == '__main__':
     for i in range(1, 6):
         new.insert(i)
     print new
-    print new[3]
+    print new[0]
     new.remove(3)
     print new
