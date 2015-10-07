a0319c941319b4213cbb82cefc10155061dc823a
azhukov <alz@unisport.dk>
2015-10-07 14:43:46 +0300

    Add deterministic uniform distribution

--- py_skiplist/iterators.py
+++ py_skiplist/iterators.py
@@ -4,3 +4,10 @@ import random
 
 def geometric(p):
     return (next(dropwhile(lambda _: random.randint(1, int(1. / p)) == 1, count())) for _ in cycle([1]))
+
+
+def uniform(n):
+    """
+    Simple deterministic distribution for testing internal of the skiplist
+    """
+    return (n for _ in cycle([1]))
