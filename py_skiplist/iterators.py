88e0ec5ff58f7dabb531749472a410498c8e7827
Joshua Bronson <jabronson@gmail.com>
2021-03-28 16:21:46 -0400

    Use itertools.repeat over slower alternatives.

--- py_skiplist/iterators.py
+++ py_skiplist/iterators.py
@@ -1,13 +1,10 @@
-from itertools import dropwhile, count, cycle
+from itertools import dropwhile, count, repeat
 import random
 
 
 def geometric(p):
-    return (next(dropwhile(lambda _: random.randint(1, int(1. / p)) == 1, count())) for _ in cycle([1]))
+    return (next(dropwhile(lambda _: random.randint(1, int(1. / p)) == 1, count())) for _ in repeat(1))
 
 
-def uniform(n):
-    """
-    Simple deterministic distribution for testing internal of the skiplist
-    """
-    return (n for _ in cycle([1]))
+# Simple deterministic distribution for testing internals of the skiplist. 
+uniform = repeat
